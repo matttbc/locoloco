7f2df76c46f5a0767adf5a338e6b9d407b1a29f9
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-11 16:01:26 +0100

    plugin authentication routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import withSuspense from '@hoc/with-suspense';
+import withAuthentication from '@hoc/with-authentication';
 
 const Landing = React.lazy(() => import('@pages/landing/components/root'));
-const BusinessRegistration = React.lazy(() => import('@pages/business-registration/components/root'));
+const AuthenticationCallback = React.lazy(() => import('@pages/authentication-callback/containers/root'));
+const BusinessRegistration = React.lazy(() => import('@pages/business-registration/containers/root'));
 
+export const AUTHENTICATION_CALLBACK_PATH = '/authentication/callback';
+export const AUTHENTICATION_RENEW_PATH = '/authentication/silent-renew';
 export const LANDING_PATH = '/';
 export const REGISTER_BUSINESS_PATH = '/register';
 
@@ -15,8 +19,13 @@ export default [
     component: withSuspense(Landing),
   },
   {
+    path: AUTHENTICATION_CALLBACK_PATH,
+    exact: true,
+    component: withSuspense(AuthenticationCallback),
+  },
+  {
     path: REGISTER_BUSINESS_PATH,
-    component: withSuspense(BusinessRegistration),
+    component: withAuthentication(withSuspense(BusinessRegistration)),
   },
   {
     path: '*',
