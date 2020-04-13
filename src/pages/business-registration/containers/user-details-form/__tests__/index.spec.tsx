d21e38cf6c1a8a47fc242655aabd10b05ae7a183
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-13 14:34:00 +0100

    create user details form container

---
+++ src/pages/business-registration/containers/user-details-form/__tests__/index.spec.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Formik } from 'formik';
+import { mount } from 'enzyme';
+import { mocked } from 'ts-jest/utils';
+
+import { useStore } from '@store';
+import UserDetailsForm from '..';
+
+jest.mock('@store', () => ({
+  useStore: jest.fn(),
+}));
+
+describe('UserDetailsForm container', () => {
+  let renderProps;
+  let trade;
+  const mockedUseStore = mocked(useStore);
+
+  beforeEach(() => {
+    renderProps = {
+      goToNextStep: jest.fn(),
+    };
+    trade = {
+      register: {
+        userDetails: {
+          firstName: 'John',
+          lastName: 'Smith',
+          role: 'owner',
+          phone: '07545673466',
+        },
+      },
+    };
+    mockedUseStore.mockClear();
+  });
+
+  describe('render', () => {
+    it('should render a UserDetailsForm component', () => {
+      mockedUseStore.mockReturnValue({ trade });
+      const wrapper = mount(<UserDetailsForm {...renderProps} />);
+      expect(wrapper.find('UserDetailsForm').length).toEqual(1);
+    });
+  });
+
+  describe('form', () => {
+    describe('initial values', () => {
+      it('should set the initial values from the store', () => {
+        mockedUseStore.mockReturnValue({ trade });
+        const wrapper = mount(<UserDetailsForm {...renderProps} />);
+        const props = wrapper.find(Formik).props() as any;
+        expect(props.initialValues).toMatchObject({
+          firstName: trade.register.userDetails.firstName,
+          lastName: trade.register.userDetails.lastName,
+          role: trade.register.userDetails.role,
+          phone: trade.register.userDetails.phone,
+        });
+      });
+    });
+
+    describe('submit', () => {
+      it('should set store register user details props and call goToNextStep prop', () => {
+        mockedUseStore.mockReturnValue({ trade });
+        const wrapper = mount(<UserDetailsForm {...renderProps} />);
+        const props = wrapper.find(Formik).props() as any;
+        const values = {
+          firstName: 'Joe',
+          lastName: 'Doe',
+          role: 'manager',
+          phone: '07456773478',
+        };
+        props.onSubmit(values);
+        expect(trade.register.userDetails).toMatchObject(values);
+        expect(renderProps.goToNextStep).toHaveBeenCalled();
+      });
+    });
+  });
+});
---
+++ src/pages/business-registration/containers/user-details-form/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { useStore } from '@store';
+import UserDetailsForm from '../../components/user-details-form';
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  role: string;
+  phone: string;
+};
+
+type Props = {
+  goToNextStep: () => void;
+};
+
+export const ValidationSchema = Yup.object().shape({
+  firstName: Yup
+    .string()
+    .required('First name is required.'),
+  lastName: Yup
+    .string()
+    .required('Last name is required.'),
+  role: Yup
+    .string()
+    .required('Role is required.'),
+  phone: Yup
+    .string()
+    .required('Phone is required.')
+    .matches(/^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/, { message: 'Phone number is not valid.' }),
+});
+
+const mapStoreToInitialValues = (store) => ({
+  firstName: store.register.userDetails.firstName,
+  lastName: store.register.userDetails.lastName,
+  role: store.register.userDetails.role,
+  phone: store.register.userDetails.phone,
+});
+
+const Form: React.FC<Props> = ({ goToNextStep }: Props) => {
+  const { trade } = useStore();
+
+  const onSubmit = (values: FormValues) => {
+    trade.register.userDetails = values;
+    goToNextStep();
+  };
+
+  return (
+    <Formik
+      initialValues={mapStoreToInitialValues(trade)}
+      validationSchema={ValidationSchema}
+      onSubmit={onSubmit}
+    >
+      {(props) => <UserDetailsForm {...props} />}
+    </Formik>
+  );
+};
+
+export default Form;
