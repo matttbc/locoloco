f895304cf7847f6b96ffe23c38bfc56e5d0645b8
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-14 10:20:07 +0100

    create business info form container

---
+++ src/pages/business-registration/containers/business-info-form/__tests__/index.spec.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Formik } from 'formik';
+import { mount } from 'enzyme';
+import { mocked } from 'ts-jest/utils';
+
+import { useStore } from '@store';
+import UserDetailsForm from '..';
+
+jest.mock('@store', () => ({
+  useStore: jest.fn(),
+}));
+
+describe('BusinessInfoForm container', () => {
+  let renderProps;
+  let trade;
+  const mockedUseStore = mocked(useStore);
+
+  beforeEach(() => {
+    renderProps = {
+      goToNextStep: jest.fn(),
+    };
+    trade = {
+      register: {
+        businessDetails: {
+          name: 'PizzaHouse',
+          postcode: 'nw1 8xl',
+          offerType: ['pantry'],
+          cuisineType: ['japanese'],
+        },
+      },
+    };
+    mockedUseStore.mockClear();
+  });
+
+  describe('render', () => {
+    it('should render a BusinessInfoForm component', () => {
+      mockedUseStore.mockReturnValue({ trade });
+      const wrapper = mount(<UserDetailsForm {...renderProps} />);
+      expect(wrapper.find('BusinessInfoForm').length).toEqual(1);
+    });
+  });
+
+  describe('form', () => {
+    describe('initial values', () => {
+      it('should set the initial values from the store', () => {
+        mockedUseStore.mockReturnValue({ trade });
+        const wrapper = mount(<UserDetailsForm {...renderProps} />);
+        const props = wrapper.find(Formik).props() as any;
+        expect(props.initialValues).toMatchObject({
+          name: trade.register.businessDetails.name,
+          postcode: trade.register.businessDetails.postcode,
+          offerType: trade.register.businessDetails.offerType,
+          cuisineType: trade.register.businessDetails.cuisineType,
+        });
+      });
+    });
+
+    describe('submit', () => {
+      it('should set store register business details props and call goToNextStep prop', () => {
+        mockedUseStore.mockReturnValue({ trade });
+        const wrapper = mount(<UserDetailsForm {...renderProps} />);
+        const props = wrapper.find(Formik).props() as any;
+        const values = {
+          name: 'PizzaHot',
+          postcode: 'nw1 0th',
+          offerType: ['delivery'],
+          cuisineType: ['chinese'],
+        };
+        props.onSubmit(values);
+        expect(trade.register.businessDetails).toMatchObject(values);
+        expect(renderProps.goToNextStep).toHaveBeenCalled();
+      });
+    });
+  });
+});
---
+++ src/pages/business-registration/containers/business-info-form/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { toJS } from 'mobx';
+
+import { useStore } from '@store';
+import BusinessInfoForm from '../../components/business-info-form';
+
+type FormValues = {
+  name: string;
+  postcode: string;
+  offerType: string[];
+  cuisineType: string[];
+};
+
+type Props = {
+  goToNextStep: () => void;
+};
+
+export const ValidationSchema = Yup.object().shape({
+  name: Yup
+    .string()
+    .required('Business name is required.'),
+  postcode: Yup
+    .string()
+    .required('Postcode is required.')
+    .matches(/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/, { message: 'Postcode is not valid.' }),
+  offerType: Yup
+    .array()
+    .required('Offer type is required.'),
+  cuisineType: Yup
+    .array()
+    .required('Cuisine type is required.'),
+});
+
+const mapStoreToInitialValues = (store) => ({
+  name: store.register.businessDetails.name,
+  postcode: store.register.businessDetails.postcode,
+  offerType: store.register.businessDetails.offerType,
+  cuisineType: store.register.businessDetails.cuisineType,
+});
+
+const Form: React.FC<Props> = ({ goToNextStep }: Props) => {
+  const { trade } = useStore();
+
+  const onSubmit = (values: FormValues) => {
+    console.log(values);
+    trade.register.businessDetails = values;
+    goToNextStep();
+  };
+
+  return (
+    <Formik
+      initialValues={mapStoreToInitialValues(toJS(trade))}
+      validationSchema={ValidationSchema}
+      onSubmit={onSubmit}
+    >
+      {(props) => <BusinessInfoForm {...props} />}
+    </Formik>
+  );
+};
+
+export default Form;
