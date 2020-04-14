956b60c7dc6c3135dd906625dde1d0574c3a691e
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-14 10:32:46 +0100

    update landing page component tests

--- src/pages/landing/components/root/__tests__/index.spec.tsx
+++ src/pages/landing/components/root/__tests__/index.spec.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Landing from '..';
+import RegisterForm from '../../../containers/register-form';
 
 describe('Landing page component', () => {
   describe('render', () => {
-    it('should render', () => {
-      expect(() => shallow(<Landing />)).not.toThrow();
+    it('should render a RegisterForm container', () => {
+      const wrapper = shallow(<Landing />);
+      expect(wrapper.find(RegisterForm).length).toEqual(1);
     });
   });
 });
