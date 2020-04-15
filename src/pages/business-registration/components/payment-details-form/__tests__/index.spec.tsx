a3eecbae7e6cba723230bd279963917c6bcb2d8f
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-15 14:23:41 +0100

    create payment details form component

---
+++ src/pages/business-registration/components/payment-details-form/__tests__/index.spec.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { FormikProps } from 'formik';
+import { Button } from '@material-ui/core';
+import { shallow } from 'enzyme';
+
+import PaymentDetailsForm, { Values } from '..';
+
+describe('PaymentDetailsForm component', () => {
+  let renderProps;
+
+  beforeEach(() => {
+    renderProps = {
+      values: {
+        type: [],
+      },
+    };
+  });
+
+  describe('render', () => {
+    it('should render a type checkbox group field', () => {
+      const wrapper = shallow(<PaymentDetailsForm {...renderProps as FormikProps<Values>} />);
+      expect(wrapper.find('FormField').at(0).props()).toMatchObject({
+        name: 'type',
+        type: 'checkboxGroup',
+        options: [
+          { label: 'Cash', value: 'cash' },
+          { label: 'Online payment', value: 'online' },
+        ],
+      });
+    });
+
+    it('should render a stripe key and secret fields if type value includes "online"', () => {
+      renderProps.values.type = ['online'];
+      const wrapper = shallow(<PaymentDetailsForm {...renderProps as FormikProps<Values>} />);
+      expect(wrapper.find('FormField').at(1).props()).toMatchObject({
+        name: 'stripe.key',
+        type: 'text',
+      });
+
+      expect(wrapper.find('FormField').at(2).props()).toMatchObject({
+        name: 'stripe.secret',
+        type: 'text',
+      });
+    });
+
+    it('should render a submit button', () => {
+      const wrapper = shallow(<PaymentDetailsForm {...renderProps as FormikProps<Values>} />);
+      expect(wrapper.find(Button).props()).toMatchObject({
+        type: 'submit',
+      });
+    });
+  });
+});
---
+++ src/pages/business-registration/components/payment-details-form/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Form, FormikProps } from 'formik';
+import { Button, Typography } from '@material-ui/core';
+
+import FormField from '@components/form-field';
+
+import styles from '@styles/forms';
+
+export type Values = {
+  type: string[];
+  stripe: {
+    key: string;
+    secret: string;
+  };
+}
+
+const PaymentDetailsForm: React.FC<FormikProps<Values>> = ({ values }: FormikProps<Values>) => {
+  const classes = styles();
+
+  return (
+    <div className={classes.formWrapper}>
+      <Typography component="h1" variant="h4" align="center" className={classes.title}>
+        Getting paid
+      </Typography>
+      <Form className={classes.form}>
+        <div className={classes.formGroup}>
+          <FormField
+            label="Which payment method do you want to accept?"
+            name="type"
+            fullWidth
+            type="checkboxGroup"
+            options={[
+              { label: 'Cash', value: 'cash' },
+              { label: 'Online payment', value: 'online' },
+            ]}
+            helpText="Select all that apply."
+          />
+        </div>
+        {values.type.includes('online') && (
+          <>
+            <div className={classes.formGroup}>
+              <Typography component="p">
+                To accept Loql Digital Payments, you will first need a Stripe Account and
+                know your Stripe Publishable Key and Secret
+              </Typography>
+              <FormField
+                label="Stripe publishable key"
+                name="stripe.key"
+                fullWidth
+                type="text"
+              />
+              <FormField
+                label="Stripe secret"
+                name="stripe.secret"
+                fullWidth
+                type="text"
+              />
+            </div>
+          </>
+        )}
+        <div className={classes.formGroup}>
+          <Button
+            type="submit"
+            color="primary"
+            size="large"
+            variant="contained"
+            className={classes.submitButton}
+          >
+            Next: Add menu info
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default PaymentDetailsForm;
