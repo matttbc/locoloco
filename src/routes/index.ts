f6cf854d73c1b291bdf0851207059a7250dafcd9
olusola <olusola2509@gmail.com>
2020-04-17 09:45:24 +0100

    added dashboard route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,11 +6,13 @@ import withAuthentication from '@hoc/with-authentication';
 const Landing = React.lazy(() => import('@pages/landing/components/root'));
 const AuthenticationCallback = React.lazy(() => import('@pages/authentication-callback/containers/root'));
 const BusinessRegistration = React.lazy(() => import('@pages/business-registration/components/root'));
-
+const Dashboard = React.lazy(() => import('@pages/dashboard/container/root'));
 export const AUTHENTICATION_CALLBACK_PATH = '/authentication/callback';
 export const AUTHENTICATION_RENEW_PATH = '/authentication/silent-renew';
 export const LANDING_PATH = '/';
 export const REGISTER_BUSINESS_PATH = '/register';
+export const DASHBOARD_PATH = '/dashboard';
+
 
 export default [
   {
@@ -26,6 +28,10 @@ export default [
   {
     path: REGISTER_BUSINESS_PATH,
     component: withAuthentication(withSuspense(BusinessRegistration)),
+  },
+  {
+    path: DASHBOARD_PATH,
+    component: withSuspense(Dashboard),
   },
   {
     path: '*',
