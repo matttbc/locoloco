cdfadbc9182e22db1b598d5155588db58d969d0c
Ionut Cirja <ionut.cirja@gmail.com>
2020-04-17 09:40:44 +0100

    update font family

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,16 +3,8 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
   typography: {
     fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
+      'Nunito Sans',
       'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
     ].join(','),
     htmlFontSize: 10,
   },
