b1213ecfaf133dce55617af905598b7c8f221942
olusola <olusola2509@gmail.com>
2020-04-17 18:47:16 +0100

    added tab functionality to dashboard

---
+++ src/pages/dashboard/containers/root/styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  dashboardWrapper: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    paddingLeft: 80,
+    paddingRight: 20,
+  },
+  drawerWidth: {
+    width: 90,
+  },
+});
