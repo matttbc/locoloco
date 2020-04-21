92530fe751a61bb1e9c8d25f94e4f7e7547ac801
olusola <olusola2509@gmail.com>
2020-04-21 13:20:35 +0100

    updated tab feature

--- src/pages/dashboard/containers/root/index.tsx
+++ src/pages/dashboard/containers/root/index.tsx
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Drawer from '@material-ui/core/Drawer';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Fastfood from '@material-ui/icons/Fastfood';
 import Euro from '@material-ui/icons/Euro';
-import PlaylistAdd from '@material-ui/icons/PlaylistAdd';
+import LocalShipping from '@material-ui/icons/LocalShipping';
+import Category from '@material-ui/icons/Category';
 import Note from '@material-ui/icons/Note';
 import Help from '@material-ui/icons/Help';
 
 import {
   ListItem, List, ListItemText, Toolbar, ListItemIcon,
 } from '@material-ui/core';
+import { DashboardRoute } from '../../../../../constants/root';
+
 
 import styles from './styles';
 
 type Props = {
   handleSelected: (text) => void;
-  routes: Obj[];
+  routes: DashboardRoutesObj[];
 };
 
-type Obj = {
+type DashboardRoutesObj = {
   label: string;
   attribute: string;
   component: string;
-  icon: string;
 };
 
-const DrawerItems: React.SFC<Props> = ({ handleSelected, routes }: Props) => (
+const DrawerItems: React.FC<Props> = ({ handleSelected, routes }: Props) => (
   <List>
-    {routes.map((ele) => (
-      <ListItem button key={ele.attribute} onClick={() => handleSelected(ele.attribute)}>
-        {ele.attribute === 'account' && <ListItemIcon><AccountCircle /></ListItemIcon>}
-        {ele.attribute === 'menu' && <ListItemIcon><PlaylistAdd /></ListItemIcon>}
-        {ele.attribute === 'payment' && <ListItemIcon><Euro /></ListItemIcon>}
-        {ele.attribute === 'orders' && <ListItemIcon><Fastfood /></ListItemIcon>}
-        {ele.attribute === 'help' && <ListItemIcon><Help /></ListItemIcon>}
-        {ele.attribute === 'policy' && <ListItemIcon><Note /></ListItemIcon>}
-        <ListItemText primary={ele.label} />
+    {routes.map((route) => (
+      <ListItem button key={route.attribute} onClick={() => handleSelected(route.attribute)}>
+        {route.attribute === 'account' && <ListItemIcon><AccountCircle /></ListItemIcon>}
+        {route.attribute === 'category' && <ListItemIcon><Category /></ListItemIcon>}
+        {route.attribute === 'menu' && <ListItemIcon><Fastfood /></ListItemIcon>}
+        {route.attribute === 'payment' && <ListItemIcon><Euro /></ListItemIcon>}
+        {route.attribute === 'orders' && <ListItemIcon><LocalShipping /></ListItemIcon>}
+        {route.attribute === 'help' && <ListItemIcon><Help /></ListItemIcon>}
+        {route.attribute === 'policy' && <ListItemIcon><Note /></ListItemIcon>}
+        <ListItemText primary={route.label} />
       </ListItem>
     ))}
   </List>
@@ -44,46 +47,7 @@ const DrawerItems: React.SFC<Props> = ({ handleSelected, routes }: Props) => (
 
 const Dashboard: React.FC<{}> = () => {
   const classes = styles();
-  const [selected, setSelected] = useState('account');
-
-  const dashboardRoute: Obj[] = [
-    {
-      label: 'Account',
-      attribute: 'account',
-      component: 'Account',
-      icon: 'AccountCircle',
-    },
-    {
-      label: 'Menu',
-      attribute: 'menu',
-      component: 'menu',
-      icon: 'Play',
-    },
-    {
-      label: 'Payment',
-      attribute: 'payment',
-      component: 'payment stuff',
-      icon: 'Euro',
-    },
-    {
-      label: 'Orders',
-      attribute: 'orders',
-      component: 'order history or current',
-      icon: 'PlaylistAdd',
-    },
-    {
-      label: 'Help',
-      attribute: 'help',
-      component: 'help email related stufff',
-      icon: 'Help',
-    },
-    {
-      label: 'Policy',
-      attribute: 'policy',
-      component: 'terms and condition related stuff',
-      icon: 'Note',
-    },
-  ];
+  const [selected, setSelected] = React.useState('account');
 
   const handleItem = (text) => {
     setSelected(text);
@@ -97,11 +61,11 @@ const Dashboard: React.FC<{}> = () => {
         open
       >
         <Toolbar />
-        <DrawerItems routes={dashboardRoute} handleSelected={handleItem} />
+        <DrawerItems routes={DashboardRoute} handleSelected={handleItem} />
       </Drawer>
       <main className={classes.content}>
         {
-          dashboardRoute.map((ele) => ele.attribute === selected && ele.component)
+          DashboardRoute.map((route) => route.attribute === selected && route.component)
         }
       </main>
     </div>
