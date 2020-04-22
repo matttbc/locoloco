b89005c07620634dc65b369577b2007eca6fe766
Dale Pepper <dale_pepper@hotmail.co.uk>
2020-04-22 17:07:15 +0100

    create legal content pages (#12)

---
+++ src/pages/legal/styles.ts
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  page: {
+    padding: '80px 16px 16px',
+  },
+  section: {
+    maxWidth: '700px',
+    marginBottom: '20px',
+  },
+  title: {
+    marginBottom: '40px',
+  },
+  info: {
+    marginBottom: '40px',
+  },
+});
---
+++ src/pages/legal/terms-and-conditions/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+
+import styles from '../styles';
+
+const TermsAndConditions: React.FC = () => {
+  const classes = styles();
+
+  return (
+    <Grid container direction="column" alignContent="center" className={classes.page}>
+      <section className={classes.section}>
+        <Typography component="h1" variant="h3" align="center" className={classes.title}>
+          Terms &amp; Conditions
+        </Typography>
+        <Typography component="p" align="center" className={classes.info}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non auctor quam.
+          Donec ac pretium ante. Donec fermentum dolor nisl, et venenatis magna accumsan sed.
+          Donec imperdiet tempor sapien, sit amet fermentum ante blandit sit amet.
+          Cras finibus elementum euismod. Nam sodales euismod diam, vel faucibus tortor
+          tincidunt et.
+          Proin id dui a nulla iaculis fringilla. Mauris cursus egestas tempus.
+          Phasellus ut nisi varius, faucibus nunc ac, congue tortor.
+          Vestibulum vel ligula eget ligula gravida malesuada.
+        </Typography>
+      </section>
+    </Grid>
+  );
+};
+
+export default TermsAndConditions;
